be73bca83aa0fdde02490898ebc40b45f63ab63a
Anuj Sharma <intrstelr.007@gmail.com>
2020-09-25 16:13:57 +0530

    [Anuj] usercase2 done

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,7 +6,16 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            int wage = DailyWage();
+            Console.WriteLine("Calculated age is "+ wage);
+        }
+
+        public static int DailyWage()
+        {
+            int FULL_DAY_HOURS = 8;
+            Random rnd = new Random();
+            int wage_calculated = rnd.Next(0, 9) * 20;
+            return wage_calculated;
         }
     }
 }
