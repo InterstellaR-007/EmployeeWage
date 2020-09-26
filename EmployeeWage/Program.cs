b3b18f1f0f6538b1b7f8477a9896918858fc1229
Anuj Sharma <intrstelr.007@gmail.com>
2020-09-26 10:20:43 +0530

    [Anuj] uc5 done

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,25 +11,33 @@ namespace EmployeeWage
         public static int EmpWage()
         {
             int empHours = 0;
-            
-            Random rnd = new Random();
-            int empCheck = rnd.Next(0, 2) ;
-            switch (empCheck)
-            {
-                case 0:
-                    empHours = 4;
-                    break;
-                case 1:
-                    empHours = 8;
-                    break;
-                default:
-                    empHours = 0;
-                    break;
-            }
-            int wageCalculated = empHours * 20;
-            
-            return wageCalculated;
+            int totalEmpWage = 0;
+            int wageCalculated = 0;
 
+            for (int i = 0; i < 20; i++)
+            {
+
+
+                Random rnd = new Random();
+                int empCheck = rnd.Next(0, 2);
+                switch (empCheck)
+                {
+                    case 0:
+                        empHours = 4;
+                        break;
+                    case 1:
+                        empHours = 8;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                wageCalculated = empHours * 20;
+                totalEmpWage = totalEmpWage + wageCalculated;
+                
+
+            }
+            return totalEmpWage;
         }
         
     }
