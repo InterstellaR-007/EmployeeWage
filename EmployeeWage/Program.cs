070292d2535a6237c98b45acacea4ed8a0c21ed6
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-28 09:33:09 +0530

    [Anuj] Emp Wage using Constructor Invoking UC9

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,9 +6,10 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Employee wage is "+ EmpWage());
+            EmpWage("abc",20,20,100);
+            EmpWage("efg", 20, 30, 120);
         }
-        public static int EmpWage()
+        public static void EmpWage(String company,int empRatePerHour, int numOfWorkingDays,int maxNumberOfHours)
         {
             int empHours = 0;
             int totalEmpHours = 0;
@@ -16,7 +17,7 @@ namespace EmployeeWage
             int wageCalculated = 0;
             int totalDays = 0;
 
-            while(totalEmpHours <=100 && totalDays <=20)
+            while(totalEmpHours <=maxNumberOfHours && totalDays <=numOfWorkingDays)
             {
                 totalDays++;
 
@@ -35,12 +36,13 @@ namespace EmployeeWage
                         break;
                 }
                 totalEmpHours = totalEmpHours + empHours;
-                wageCalculated = empHours * 20;
+                wageCalculated = empHours * empRatePerHour;
                 totalEmpWage = totalEmpWage + wageCalculated;
                 
 
             }
-            return totalEmpWage;
+            Console.WriteLine(" Employee wage of Company "+ company +" is: "+ totalEmpWage);
+            
         }
         
     }
