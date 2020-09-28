384d7aecbe87d34961b9ddf77d978461426e12cc
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-28 11:53:26 +0530

    [Anuj] Employee Wage using Object Declaration

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,44 +6,13 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            EmpWage("abc",20,20,100);
-            EmpWage("efg", 20, 30, 120);
+            empwage_new emp1 = new empwage_new("abc", 20, 20, 100);
+            emp1.EmpWage();
+            empwage_new emp2 = new empwage_new("thg", 10, 20, 10);
+            emp2.EmpWage();
+
         }
-        public static void EmpWage(String company,int empRatePerHour, int numOfWorkingDays,int maxNumberOfHours)
-        {
-            int empHours = 0;
-            int totalEmpHours = 0;
-            int totalEmpWage = 0;
-            int wageCalculated = 0;
-            int totalDays = 0;
 
-            while(totalEmpHours <=maxNumberOfHours && totalDays <=numOfWorkingDays)
-            {
-                totalDays++;
 
-                Random rnd = new Random();
-                int empCheck = rnd.Next(0, 2);
-                switch (empCheck)
-                {
-                    case 0:
-                        empHours = 4;
-                        break;
-                    case 1:
-                        empHours = 8;
-                        break;
-                    default:
-                        empHours = 0;
-                        break;
-                }
-                totalEmpHours = totalEmpHours + empHours;
-                wageCalculated = empHours * empRatePerHour;
-                totalEmpWage = totalEmpWage + wageCalculated;
-                
-
-            }
-            Console.WriteLine(" Employee wage of Company "+ company +" is: "+ totalEmpWage);
-            
-        }
-        
     }
 }
---
+++ EmployeeWage/empwage_new.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class empwage_new
+    {
+
+        private String current_company;
+        private int current_empRatePerHour;
+        private int current_numOfWorkingDays = 0;
+        private int current_maxNumberOfHours = 0;
+
+
+        public empwage_new(String company, int empRatePerHour, int numOfWorkingDays, int maxNumberOfHours)
+        {
+            current_company = company;
+            current_empRatePerHour = empRatePerHour;
+            current_numOfWorkingDays = numOfWorkingDays;
+            current_maxNumberOfHours = maxNumberOfHours;
+        }
+        public void EmpWage()
+        {
+            int empHours = 0;
+            int totalEmpHours = 0;
+            int totalEmpWage = 0;
+            int wageCalculated = 0;
+            int totalDays = 0;
+
+            while (totalEmpHours <= current_maxNumberOfHours && totalDays <= current_numOfWorkingDays)
+            {
+                totalDays++;
+
+                Random rnd = new Random();
+                int empCheck = rnd.Next(0, 2);
+                switch (empCheck)
+                {
+                    case 0:
+                        empHours = 4;
+                        break;
+                    case 1:
+                        empHours = 8;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                totalEmpHours = totalEmpHours + empHours;
+                wageCalculated = empHours * current_empRatePerHour;
+                totalEmpWage = totalEmpWage + wageCalculated;
+
+
+            }
+            Console.WriteLine(" Employee wage of Company " + current_company + " is: " + totalEmpWage);
+
+        }
+    }
+}
