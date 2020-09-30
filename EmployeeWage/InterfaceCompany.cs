4aaa32642d337af28af1f20fd6a8cd7a776717ed
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-30 09:23:13 +0530

    [Anuj] Refactor. Updated with Interface

---
+++ EmployeeWage/InterfaceCompany.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    public interface InterfaceCompany
+    {
+        public void addCompanyDetails(String company, int empRatePerHour, int numOfWorkingDays, int maxNumberOfHours);
+
+        public void computeEmpWage();
+
+        public void getEmpWage(String company);
+
+
+
+    }
+}
